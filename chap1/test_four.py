dfe50ca5d4effbd5739dbf5ee22ef3e15a47573b
junya <hatiro7code@gmail.com>
2018-11-04 17:28:58 +0900

    20181104 namedtuple _asdict _replace checked

---
+++ chap1/test_four.py
@@ -0,0 +1,24 @@
+from collections import namedtuple
+
+Task = namedtuple("Task", ["summary", "owner", "done", "id"])
+Task.__new__.__defaults__ = (None, None, False, None)
+
+
+def test_asdict():
+    """asdict() should return a dictionary"""
+    t_task = Task("do something", "okken", True, 21)
+    t_dict = t_task._asdict()  # フィールド名を対応する値にマッピングする新しい:class:OrderedDict を返します:
+    print(t_dict)
+    expected = {"summary": "do something",
+                "owner": "okken",
+                "done": True,
+                "id": 21}
+    assert t_dict == expected
+
+
+def test_replace():
+    """__replace() should change passed in fields."""
+    t_before = Task("finish book", "brian", False)
+    t_after = t_before._replace(id=10, done=True)  # _replacenamedtupleのフィールドの値を入れ替える
+    t_expected= Task("finish book", "brian", True, 10)
+    assert t_after == t_expected
