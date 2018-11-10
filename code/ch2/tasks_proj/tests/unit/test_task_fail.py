76a8774e406694291963b2644800901392331c02
junya <hatiro7code@gmail.com>
2018-11-10 18:03:57 +0900

    4func test fail

---
+++ code/ch2/tasks_proj/tests/unit/test_task_fail.py
@@ -0,0 +1,16 @@
+from tasks import Task
+
+
+def test_task_equalityu():
+    """Different tasks should not be equal"""
+    t1 = Task("sit there", "brian")
+    t2 = Task("do something", "okken")
+    assert t1 == t2
+
+
+def test_dict_equality():
+    """Different tasks compared as dicts should not be equal."""
+    t1_dict = Task("make sandwitch", "okken")._asdict()
+    t2_dict = Task("make sandwitch", "okkem")._asdict()
+    assert t1_dict == t2_dict
+    
